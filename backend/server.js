c06ae8d1255a0cf22e7282b84326901bf7f3aad3
dineshsy <s.ydinesh1177@gmail.com>
2020-08-30 14:45:58 +0530

    Fix: CORS

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+//CORS middleware
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+
+    next()
+}
+app.use(allowCrossDomain)
 app.use('/api', Restaurants)
 
 //Connect to Mongo
